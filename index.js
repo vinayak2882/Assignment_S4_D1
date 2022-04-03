9f21a7239867b9d5279e7c16e48dbebad92caae2
vinayak2882 <vinayak.patrut@gmail.com>
2022-04-03 20:04:31 +0530

    done

---
+++ index.js
@@ -0,0 +1,41 @@
+const mongoose=require("mongoose");
+const express= require("express");
+const app= express();
+const {register, login, newToken}= require("./src/controllers/authController")
+
+app.use(express.json());
+
+
+const passport= require("./src/config/oauthconfig")
+
+const prodController= require("./src/controllers/prodController");
+
+
+app.use("/products", prodController);
+
+app.post("/register", register);
+
+app.post("/login", login);
+
+// app.post("/products", products);
+
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile','email'] }));
+ 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' , session:false}),
+  function(req, res) {
+    const token= newToken(req.user);
+    return res.status(200).send({user:req.user, token})
+  });
+
+
+app.listen(5000, async()=>{
+    try {
+        await connect();
+    } catch (error) {
+        console.log(error)
+    }
+    console.log("Listening to port 5000");
+})
